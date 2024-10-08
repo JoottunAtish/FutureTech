0bc011b25156d3a1750b587bd5f9344b2109e21b
JoottunAtish <ajoottun24@gmail.com>
2024-10-08 09:19:44 +0400

    completing the search function, fixing minor bugs

--- homepage.php
+++ homepage.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Menu\style-nav.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js" integrity="sha512-+k1pnlgt4F1H8L7t3z95o3/KO+o78INEcXTbnoJQ/F2VqDVhWoaiVml/OEHv9HsVgxUaVW+IbiZPUJQfF/YxZw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <title>FutureTech - Homepage</title>
 </head>
 <body>
@@ -42,7 +42,7 @@
                     }
                 });
             } else {
-                $(".card-container").css("display", "none");
+                $(".card-container").html("");
             }
         });
     });
--- product-search/live-search.php
+++ product-search/live-search.php
@@ -6,16 +6,26 @@
 
         $user_in = addslashes($user_in);
 
-        $query = "SELECT * FROM `product` WHERE ProductName LIKE %$user_in% OR  Description LIKE %$user_in%" ;
+        $query = "SELECT * FROM `product` WHERE ProductName LIKE '%{$user_in}%' OR  Description LIKE '%{$user_in}%'" ;
 
         $res = $conn->prepare($query);
         $res->execute();
 
-        if (res->rowCount() > 0){
-            $result = res->fetch(PDO::FETCH_ASSOC);
+        if ($res->rowCount() > 0){
+            while($result = $res->fetch(PDO::FETCH_ASSOC)){
 
+                $pid = $result['ProductID'];
+                $Pname = $result['ProductName'];
+                $Pprice = $result['ProductPrice'];
+                ?>
+                    <p><?php echo "$pid --> $Pname --> $Pprice"; ?></p>
+                <?php
+            }
             
+        } else{
+            echo "Nothing to display!";
         }
+
 
     }
 ?>
